56a640bf1a2d373188fc23ff35e4099fda5f7d89
Charlotte Fouques <charlottefouques10@gmail.com>
2023-08-04 16:19:42 +0200

    modif

--- test.js
+++ test.js
@@ -17,36 +17,27 @@ $(document).ready(function () {
       $.each(data, function (index, item) {
         $(".navbar-nav").append(
           "<li class='nav-item fs-3 mb-4'><a class='nav-link " +
-            item.nom +
+            item.id +
             "' href='#' >" +
             item.nom +
             "</a></li>"
         );
         // création des divs pour submenu
         $(".sub-menu").append(
-          '<div class="sous-menu ' +
-            item.nom +
+          '<div class="sous-menu menu-' +
+            item.id +
             '"><div class="nav-container"><h2>Sélection</h2> <ul class="submenu-list selection"></ul></div><div class="nav-container"><h2>Marques</h2> <ul class="submenu-list marques"> </ul></div><div class="nav-container"><h2>Région</h2> <ul class="submenu-list region"> </ul></div></div>'
         );
         // Boucles pour afficher un par un les éléments de selection
-        if (item.selection) {
-          console.log(item.selection);
-          for (select of item.selection) {
-            console.log(select);
+        if (item.selection && item.id === "scotch") {
+          $.each(item.selection, function (i, select) {
             $(".selection").append("<li>" + select + "</li>");
-          }
+          });
         }
-        // test pour afficher les marques du premier element JSON
-        if (item.marques) {
-          for (var i = 0; i < item.marques.length; i++) {
-            $(".marques").append("<li>" + item.marques[i] + "</li>");
-          }
-        }
-        // test pour afficher les marques du premier element JSON
-        if (item.region) {
-          for (var i = 0; i < item.region.length; i++) {
-            $(".region").append("<li>" + item.region[i] + "</li>");
-          }
+        if (item.selection && item.id === "japonais") {
+          $.each(item.selection, function (i, select) {
+            $(".selection").append("<li>" + select + "</li>");
+          });
         }
       });
     },
